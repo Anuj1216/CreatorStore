d923ca2754ad054b16112ac3c899511bf7e1de31
Anuj1216 <acharyaanuj45@gmail.com>
2026-07-24 15:01:34 +0545

    order module completed

--- src/main/java/com/anuj/creatorstore/entities/OrderItem.java
+++ src/main/java/com/anuj/creatorstore/entities/OrderItem.java
@@ -2,10 +2,7 @@ package com.anuj.creatorstore.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -15,6 +12,7 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderItem {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
